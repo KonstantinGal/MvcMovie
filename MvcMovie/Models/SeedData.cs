8ad7e02cb2bd93b5ff58589f15d0ea3ed9e2ded3
Konstantin Galkin <galkink@my.erau.edu>
2019-05-11 23:22:15 -0400

    Complete

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -27,6 +27,7 @@ namespace MvcMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
+                        Rating = "R",
                         Price = 7.99M
                     },
 
@@ -35,6 +36,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 8.99M
                     },
 
@@ -43,7 +45,17 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-1-11"),
+                        Genre = "Romantic Comedy",
+                        Rating = "R",
+                        Price = 7.99M
                     },
 
                     new Movie
@@ -51,6 +63,7 @@ namespace MvcMovie.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
+                        Rating = "R",
                         Price = 3.99M
                     }
                 );
